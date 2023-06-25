2d695f0ab4a1acd6edabc08464d590c3606efe6a
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-06-25 18:12:05 +0200

    [Update]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
+import { AngularFireDatabase} from '@angular/fire/compat/database';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Recruiter-app';
+  count = signal<number>(0);
+  constructor(private db: AngularFireDatabase) {
+    effect(()=> {
+      console.log("effect " + this.count());
+    });
+  }
+
+  changeCount(val:number) {
+    this.count.set(this.count() + val);
+
+    const tutRef = this.db.object('tutorial');
+    tutRef.set({ title: val });
+
+  }
+  
 }
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { environment } from 'src/environments/environment';
+import { AngularFireModule } from "@angular/fire/compat";
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
