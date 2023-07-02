2639ebc6edd29ce5eca05962e997d1e0df398f12
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-07-02 16:33:52 +0200

    [WIP]

--- src/app/services/profile.service.ts
+++ src/app/services/profile.service.ts
@@ -13,8 +13,8 @@ export class ProfileService {
 
   public async getProfile(offset: number= 0, limit:number = 8) : Promise<IProfile[] | null> {
 
-    // const { data, error } = await this.supabase.getSupabaseClient().rpc('get_user_content_creators_with_networks');
-    // console.log(data, error);
+    const { data, error } = await this.supabase.getSupabaseClient().rpc('get_user_content_creator_details', { offset_val: offset, page_size: limit });
+    console.log(data, error);
 
     let profilesList: PostgrestSingleResponse<any[]>= await this.supabase.getSupabaseClient().from(TABLE_PROFILE).select("*").range(offset, limit - 1);
     return profilesList.data as unknown as IProfile[];
---
+++ supabase/migrations/20230702141716_function_get_user_content_creator.sql
@@ -0,0 +1,39 @@
+DROP FUNCTION get_user_content_creator_details();
+DROP TYPE user_content_creator_type;
+
+CREATE TYPE user_content_creator_type AS (
+    id INT,
+    firstName VARCHAR,
+    email VARCHAR,
+    pic_url TEXT,
+    phoneNumber VARCHAR,
+    payment_names TEXT[],
+    niches TEXT[],
+    networks_names VARCHAR[],
+    networks_urls TEXT[],
+    networks_global_stats_views BIGINT[]
+);
+
+CREATE OR REPLACE FUNCTION get_user_content_creator_details() 
+RETURNS SETOF user_content_creator_type AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+        ucc.id,
+        ucc.firstName,
+        ucc.email,
+        ucc.pic_url,
+        ucc.phoneNumber,
+        ucc.payment_names,
+        ucc.niches,
+        array_agg(COALESCE(n.network_name, '')),  -- replace NULL with an empty string
+        array_agg(COALESCE(n.network_url, '')),  -- replace NULL with an empty string
+        array_agg(COALESCE(n.global_stats_view, 0))  -- replace NULL with zero
+    FROM
+        user_content_creator AS ucc
+    JOIN
+        networks AS n ON ucc.id = n.user_content_creator_id
+    GROUP BY
+        ucc.id;
+END;
+$$ LANGUAGE plpgsql;
